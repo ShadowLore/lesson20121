d8bdc0393c5bb33e29b1156cec0a1a06ca05a1ab
nfsri <nfsrivals757@gmail.com>
2020-12-01 11:26:11 +0300

    step_4

--- step_4.py
+++ step_4.py
@@ -1,6 +1,12 @@
 group_28 = ['Данила', 'Илья', 'Георгий', 'Соня', 'Никита', 'Влад', 'Катя']
 #               0        1         2       3          4       5       6
+#                                                      -2        -1        0
 
-print(group_28[2])
+print(group_28[2])  # hardcore
 # len(group_28) -> 6
-print(group_28[len(group_28) - 1])
+
+print(group_28[len(group_28) - 1])  # not ok
+print(group_28[-1])  # ok
+
+print(group_28[-4])
+print(group_28[len(group_28) - 4])
